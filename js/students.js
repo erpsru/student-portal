6867885f382ef59415e2b6c1de4851914273a23f
rajveersinghjpg <prepwithraj@gmail.com>
2026-02-07 11:34:50 +0530

    Add student information to students.js
    Added student data for multiple students including personal details and course information.

---
+++ js/students.js
@@ -0,0 +1,54 @@
+const students = {
+
+"SRU220740087": {
+    name: "RUPESH KUMAR SINGH",
+    father: "SANTOSH KUMAR SINGH",
+    mother: "SANDHYA DEVI",
+    session: "JULY-2022",
+    course: "BACHELOR OF SCIENCE",
+    stream: "PCM (NEP)",
+    category: "GENERAL",
+    gender: "MALE",
+    year: "1",
+    address: "VILL - TIWARPUR",
+    district: "VARANASI",
+    state: "UTTAR PRADESH",
+    country: "INDIA",
+    photo: "images/rupesh.jpg"
+},
+
+"36852": {
+    name: "ARCHIT KUMAR",
+    father: "BRHAM PAL",
+    mother: "-",
+    session: "2025-26",
+    course: "DIPLOMA IN ENGG (AUTOMOBILE)",
+    stream: "",
+    category: "GENERAL",
+    gender: "MALE",
+    year: "2",
+    address: "",
+    district: "",
+    state: "",
+    country: "INDIA",
+    photo: "images/archit.jpg"
+},
+
+"37652": {
+    name: "ABHISHEK PAIL WAR",
+    father: "MAHAVEER SINGH",
+    mother: "-",
+    session: "2025-26",
+    course: "MASTER OF BUSINESS ADMINISTRATION",
+    stream: "MARKETING & HR",
+    category: "GENERAL",
+    gender: "MALE",
+    year: "1",
+    address: "",
+    district: "",
+    state: "",
+    country: "INDIA",
+    photo: "images/abhishek.jpg"
+}
+
+};
